1fe5760897f8f8e0e2ba224eb9dc3d80eec494fe
Jonthan <Jonthan.LT@gmail.com>
2015-12-14 13:52:09 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/copythat/copythat/MainActivity.java
+++ app/src/main/java/com/example/copythat/copythat/MainActivity.java
@@ -100,43 +100,43 @@ public class MainActivity extends Activity {
                             e.printStackTrace();
                         }
                         try {
-                            fos.write('R');                 // 00 - RIFF
-                            fos.write('I');                 // 00 - RIFF
-                            fos.write('F');                 // 00 - RIFF
-                            fos.write('F');                 // 00 - RIFF
-                            fos.write((byte)((MainActivity.FullBuffer.length + 36) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)(((MainActivity.FullBuffer.length + 36) >> 8) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)(((MainActivity.FullBuffer.length + 36) >> 16) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)(((MainActivity.FullBuffer.length + 36) >> 24) & 0xff)); // 0 - size of this chunk
-                            fos.write('W');                 // 00 - RIFF
-                            fos.write('A');                 // 00 - RIFF
-                            fos.write('V');                 // 00 - RIFF
-                            fos.write('E');                 // 00 - RIFF
-                            fos.write('f');                 // 00 - RIFF
-                            fos.write('m');                 // 00 - RIFF
-                            fos.write('t');                 // 00 - RIFF
-                            fos.write(' ');                 // 00 - RIFF
-                            fos.write(16); // 16 - size of this chunk
-                            fos.write(0); // 0 - size of this chunk
-                            fos.write(0); // 0 - size of this chunk
-                            fos.write(0); // 0 - size of this chunk
-                            fos.write(1); // 0 - size of this chunk
-                            fos.write(0); // 0 - size of this chunk
-                            fos.write(2); // 0 - size of this chunk
-                            fos.write(0); // 0 - size of this chunk
-                            fos.write((byte)(44100 & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)((44100 >> 8) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)((44100 >> 16) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)((44100 >> 24) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)(88200 & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)((88200 >> 8) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)((88200 >> 16) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)((88200 >> 24) & 0xff)); // 0 - size of this chunk
-                            fos.write((byte)(4)); // 0 - size of this chunk
-                            fos.write(0); // 0 - size of this chunk
-                            fos.write((byte)16);        // 20 - what is the audio format? 1 for PCM = Pulse Code Modulation
-                            fos.write(0); // 0 - size of this chunk
-                            fos.write('d');                 // 00 - RIFF
+                            fos.write('R');
+                            fos.write('I');
+                            fos.write('F');
+                            fos.write('F');
+                            fos.write((byte)((MainActivity.FullBuffer.length + 36) & 0xff));
+                            fos.write((byte)(((MainActivity.FullBuffer.length + 36) >> 8) & 0xff));
+                            fos.write((byte)(((MainActivity.FullBuffer.length + 36) >> 16) & 0xff));
+                            fos.write((byte)(((MainActivity.FullBuffer.length + 36) >> 24) & 0xff));
+                            fos.write('W');
+                            fos.write('A');
+                            fos.write('V');
+                            fos.write('E');
+                            fos.write('f');
+                            fos.write('m');
+                            fos.write('t');
+                            fos.write(' ');
+                            fos.write(16);
+                            fos.write(0);
+                            fos.write(0);
+                            fos.write(0);
+                            fos.write(1);
+                            fos.write(0);
+                            fos.write(2);
+                            fos.write(0);
+                            fos.write((byte)(44100 & 0xff));
+                            fos.write((byte)((44100 >> 8) & 0xff));
+                            fos.write((byte)((44100 >> 16) & 0xff));
+                            fos.write((byte)((44100 >> 24) & 0xff));
+                            fos.write((byte)(88200 & 0xff));
+                            fos.write((byte)((88200 >> 8) & 0xff));
+                            fos.write((byte)((88200 >> 16) & 0xff));
+                            fos.write((byte)((88200 >> 24) & 0xff));
+                            fos.write((byte)(4));
+                            fos.write(0);
+                            fos.write((byte)16);
+                            fos.write(0);
+                            fos.write('d');
                             fos.write('a');                 // 00 - RIFF
                             fos.write('t');                 // 00 - RIFF
                             fos.write('a');                 // 00 - RIFF
